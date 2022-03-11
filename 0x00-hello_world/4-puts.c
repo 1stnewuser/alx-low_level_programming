758edaedd9cfaf961708fb2cd25364ebd191fd7a
1stnewuser <ngozichukwufrancis@gmail.com>
2022-03-11 02:47:46 +0100

    task 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-nrjewk
+#include<stdio.h>
+7*
+*main-Entry point
+*
+* Return:Always 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual pizzle");
+return (0);
+}
